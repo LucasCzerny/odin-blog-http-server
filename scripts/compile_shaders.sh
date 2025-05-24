f124a474b1418cbaa73e5bb2a2a13c34629d780d
Lucas Czerny <czernylucas@gmail.com>
2025-05-24 10:49:11 +0200

    first blog post almost done, website design done

--- scripts/compile_shaders.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-glslc shaders/path_tracing.comp -o shaders/path_tracing.comp.spv
-glslc shaders/post_process.vert -o shaders/post_process.vert.spv
-glslc shaders/post_process.frag -o shaders/post_process.frag.spv
